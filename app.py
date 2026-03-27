e4dcd3ead0cdb84aba6b2f5e35c8c2d613958aff
BradR91 <70640295+BradR91@users.noreply.github.com>
2026-03-27 22:14:40 +0000

    update app.py to include DB seed data

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import models
 from models import db, Role
-from repositories import RoleRepository, UserRepository
+from repositories import RoleRepository, UserRepository, PartRepository, PrefixRepository
 from controllers.auth_controller import auth_bp
 from controllers.parts_controller import parts_bp
 from controllers.prefix_controller import prefix_bp
@@ -31,8 +31,55 @@ def create_app():
         if not RoleRepository.get_by_id(2):
             db.session.add(Role(ID=2, RoleDesc='Admin'))
 
+        if not UserRepository.get_by_username('regular'):
+            UserRepository.create('regular', 'password', 1)
+
         if not UserRepository.get_by_username('admin'):
             UserRepository.create('admin', 'password1', 2)
+
+        if not PartRepository.get_by_part_no('SABBTTC5359'):
+            part1 = PartRepository.create('SABBTTC5359', 'Super Hub 3')
+            PrefixRepository.create(part1.ID, '3172J7650')
+
+        if not PartRepository.get_by_part_no('SABTTC5364'):
+            part2 = PartRepository.create('SABTTC5364', 'Hub 3')
+            PrefixRepository.create(part2.ID, '3772469890')
+            PrefixRepository.create(part2.ID, '3727447888')
+
+        if not PartRepository.get_by_part_no('TTDLINK2780'):
+            part3 = PartRepository.create('TTDLINK2780', 'Hub 1')
+            PrefixRepository.create(part3.ID, '2848B3421')
+            PrefixRepository.create(part3.ID, '2849C7550')
+
+        if not PartRepository.get_by_part_no('SABBTTB5359'):
+            part4 = PartRepository.create('SABBTTB5359', 'Business Super Hub 3')
+            PrefixRepository.create(part4.ID, '3172J7650')
+
+        if not PartRepository.get_by_part_no('SABTTB5364'):
+            part5 = PartRepository.create('SABTTB5364', 'Business Hub 3')
+            PrefixRepository.create(part5.ID, '3772469890')
+            PrefixRepository.create(part5.ID, '3727447888')
+
+        if not PartRepository.get_by_part_no('TTBDLINK2780'):
+            part6 = PartRepository.create('TTBDLINK2780', 'Business Hub 1')
+            PrefixRepository.create(part6.ID, '3551E8892')
+        
+        if not PartRepository.get_by_part_no('TTHUAWEI522'):
+            part7 = PartRepository.create('TTHUAWEI522', 'Legacy Hub 1')
+            PrefixRepository.create(part7.ID, '1923F5667')
+        
+        if not PartRepository.get_by_part_no('TTBHUAWEI532'):
+            part8 = PartRepository.create('TTBHUAWEI532', 'Legacy Business Hub 1')
+            PrefixRepository.create(part8.ID, '2034G7779')
+            PrefixRepository.create(part8.ID, '2035H8881')
+        
+        if not PartRepository.get_by_part_no('TTDLINK1780'):
+            part9 = PartRepository.create('TTDLINK1780', 'Legacy Hub 1')
+            PrefixRepository.create(part9.ID, '1721K2334')
+
+        if not PartRepository.get_by_part_no('TTEERO6'):
+            part10 = PartRepository.create('TTEERO6', 'Mesh Hub 1')
+            PrefixRepository.create(part10.ID, '4562L5556')
 
         db.session.commit()
 
